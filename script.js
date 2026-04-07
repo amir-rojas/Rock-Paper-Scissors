b5cd73f809a4a4ea1b4e7b290b04182ff541061c
amir-rojas <amir.rojas@ucb.edu.bo>
2026-04-07 11:47:51 -0400

    Add function to play a round

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-console.log("hello world!")
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
-    let choice = Math.floor(Math.random() * 3);
+    const choice = Math.floor(Math.random() * 3);
     if (choice === 0){
-        return "Rock";
+        return "rock";
     } else if (choice === 1){
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors";
+        return "scissors";
     }   
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("Rock, paper or scissors?")
+    const humanChoice = prompt("Rock, paper or scissors?")
     return humanChoice;
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice){
+    const player = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+    console.log(humanChoice)
+    console.log(computerChoice)
+    
+    if (player === computer){
+        return "It is a tie!"
+    }
+
+    const winsAgainst = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+    
+    if (winsAgainst[player] === computer){
+        humanScore++;
+        return `You win ${player} beats ${computer}!`;
+    } else {
+        computerScore++;
+        return `You lose ${computer} beats ${player}!`;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(`Human score ${humanScore} Computer Score ${computerScore}` )
